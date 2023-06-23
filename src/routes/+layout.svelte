367dae969bfd37578c193235c9e24f5cd2abcbf5
Ian Hogers <ihogers@nexigen.digital>
2023-06-23 17:00:27 +1000

    test commit new font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    // Supports weights 100-800
+    import "@fontsource-variable/jetbrains-mono";
+</script>
+
+<slot />
+
+<style>
+    :global(body) {
+        font-family: "JetBrains Mono Variable", sans-serif;
+    }
+</style>
